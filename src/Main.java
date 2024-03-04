e4ad497a95e9381f33a3be2f9ef8ca7f63333e4f
Dal S Garbuja Pun <n11176491@qut.edu.au>
2024-03-04 23:55:00 +1000

    Added a rectangle

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main{
         System.out.println("Hello, world!");
         System.out.println("Welcome to CAB302!");
         System.out.println("This is our first project!");
+        for(int i = 0; i < 5; i++) {
+            System.out.println("************");
+        }
     }
 }
