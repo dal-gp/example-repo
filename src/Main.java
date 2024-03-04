be7a5c8a341e809a8f11d8313e6416afd7a3f2a7
Dal S Garbuja Pun <n11176491@qut.edu.au>
2024-03-05 07:47:49 +1000

    Revert "Merged changes from rectangle and triangle branches"
    This reverts commit 72324c5463a55a1783be09c078fc356ba8f87f0d, reversing
    changes made to cfda3aa1493d0b297c9af35590f4f6d7e76bdc30.

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,8 @@ public class Main{
         System.out.println(leftPad("Hello, world!", 30, ' '));
         System.out.println("Welcome to CAB302!");
         System.out.println("This is our first project!");
-        for(int i = 0; i < 5; i++) {
+        for(int i = 0; i < 5; i++){
             System.out.println("************");
-        }
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < (4 - i) * 2; j++) {
-                System.out.print(" ");
-            }
-            for(int j = 0; j < i * 4 + 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
         }
     }
     public static String leftPad(String str, int len, char ch) {
